644126f8ddbbff76c1a9f4b46afb1d6611352f64
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2018-11-14 14:51:22 +0400

    Update website_portfolio_model.py

--- sh_website_portfolio/models/website_portfolio_model.py
+++ sh_website_portfolio/models/website_portfolio_model.py
@@ -9,8 +9,8 @@ class website_portfolio(models.Model):
     
     category_id=fields.Many2one("website.portfolio.category", string="Category", required=True)
     img=fields.Binary(string="Image",help="preffered size 300x300")
-    name=fields.Char(string="Title", required=True, translatable=True)
-    desc=fields.Text(string="Description", translatable=True)
+    name=fields.Char(string="Title", required=True, translate=True)
+    desc=fields.Text(string="Description", translate=True)
     is_active=fields.Boolean(string="Active", default=True)
     website_id = fields.Many2one("website", related="category_id.website_id", required=True, readonly = True)       
     
