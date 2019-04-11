b606039cc49048e11003b63e93387cb7ffe9c328
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-11 18:33:12 +0400

    fix

--- accr_scheduler/__manifest__.py
+++ accr_scheduler/__manifest__.py
@@ -11,10 +11,12 @@
     'data': [
         'security/students_scheduler_security.xml',
         'security/ir.model.access.csv',
-        'menus/students_scheduler_menu.xml',
         'views/accr_timing_view.xml',
         'views/accr_timetable_view.xml',
+        'report/timetable_report_generate.xml'
+        'report/report_menu.xml'
         'wizard/accr_generate_timetable_view.xml',
         'wizard/accr_time_table_report.xml',
+        'menus/students_scheduler_menu.xml',
     ],
 }
