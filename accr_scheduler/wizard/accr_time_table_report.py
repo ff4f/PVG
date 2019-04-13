6f623ce36d97a73d0636d52639d26d19d2432d33
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-13 15:27:04 +0400

    fix

--- accr_scheduler/wizard/accr_time_table_report.py
+++ accr_scheduler/wizard/accr_time_table_report.py
@@ -43,7 +43,7 @@ class SessionReport(models.TransientModel):
             'accr_scheduler.report_timetable_generate')
         data = self.read(
             ['start_date', 'end_date', 'section', 'timing_type'])[0]
-        timing_type = dict(self._fields['timing_type'].selection).get(self.timing_type)
+        timing_type = self.read(['timing_type'])
         if timing_type == 'all':
             time_table_ids = self.env['accr.session'].search(
                 [('section', '=', data['section'][0]),
