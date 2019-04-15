a055a83bd356611bbfd5e1b0944777c4ec3145e6
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-15 17:45:59 +0400

    fix

--- accr_nutrition/models/accr_student_nutrition_details.py
+++ accr_nutrition/models/accr_student_nutrition_details.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, exceptions, tools 
+from odoo import models, fields, api, _
 
 class accrStudentNutritionDetails(models.Model):
     _name = "accr.student.nutrition.details"
@@ -9,19 +9,19 @@ class accrStudentNutritionDetails(models.Model):
         comodel_name='model.x_student',
         ondelete='set restrict',
     )
-    name = fields.Char('Name', related='student.x_name',)
-    age = fields.Integer(related='student.x_studio_age', string="Age", readonly=True,)
-    diagnosis = fields.Char(related='student.x_studio_diagnosis', string='Diagnosis', readonly=True,)
-    residential_section = fields.Many2one(related='student.x_studio_residential_section', string='Residential Section', readonly=True,)
-    medications = fields.One2many(related='student.x_studio_field_jm5yW', string='Medications', readonly=True,)
-    food_preferences = fields.One2many('accr.food.preferences', 'student_nutrition_details', 'Food Preferences',)
+    name = fields.Char(related='student.x_name', string='Name', readonly=True, )
+    age = fields.Integer(related='student.x_studio_age', string="Age", readonly=True, )
+    diagnosis = fields.Char(related='student.x_studio_diagnosis', string='Diagnosis', readonly=True, )
+    residential_section = fields.Many2one(related='student.x_studio_residential_section', string='Residential Section', readonly=True, )
+    medications = fields.One2many(related='student.x_studio_field_jm5yW', string='Medications', readonly=True, )
+    food_preferences = fields.One2many('accr.food.preferences', 'student_nutrition_details', 'Food Preferences', )
 
-    height = fields.Integer('Height', required=True,)
-    weight = fields.Integer('Weight', required=True,)
-    diet = fields.Char('Diet',)
-    requirements = fields.Char('Requirements',)
-    physical_activity = fields.Char('Physical Activity',)
-    water_intake = fields.Char('Water Intake',)
+    height = fields.Integer('Height', required=True, )
+    weight = fields.Integer('Weight', required=True, )
+    diet = fields.Char('Diet', )
+    requirements = fields.Char('Requirements', )
+    physical_activity = fields.Char('Physical Activity', )
+    water_intake = fields.Char('Water Intake', )
 
 
 
