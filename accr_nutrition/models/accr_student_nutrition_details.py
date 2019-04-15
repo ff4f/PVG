055a43973a7294bb9be513e32dacca45cadd4cfe
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-15 20:54:06 +0400

    fix

--- accr_nutrition/models/accr_student_nutrition_details.py
+++ accr_nutrition/models/accr_student_nutrition_details.py
@@ -5,12 +5,11 @@ class accrStudentNutritionDetails(models.Model):
     _description = "Student Nutrition Details"
     
     student = fields.Many2one('x_student', 'Student', required=True, ondelete='set restrict', )
-    name = fields.Char(related=u'student.display_name', string='Name', readonly=True, )
-    age = fields.Integer(related=u'student.x_studio_age', string="Age", readonly=True, )
-    diagnosis = fields.Text(related=u'student.x_studio_diagnosis', string='Diagnosis', readonly=True, )
-    residential_section = fields.Many2one(related=u'student.x_studio_residential_section', string='Residential Section', readonly=True, )
-    medications = fields.One2many(related=u'student.x_studio_field_jm5yW', string='Medications', readonly=True, )
-    
+    name = fields.Char(related='student.display_name', string='Name', readonly=True, )
+    age = fields.Integer(related='student.x_studio_age', string="Age", readonly=True, )
+    diagnosis = fields.Text(related='student.x_studio_diagnosis', string='Diagnosis', readonly=True, )
+    residential_section = fields.Many2one(related='student.x_studio_residential_section', string='Residential Section', readonly=True, )
+    medications = fields.One2many(related='student.x_studio_field_jm5yW', string='Medications', readonly=True, )
     food_preferences = fields.One2many('accr.food.preferences', 'student_nutrition_details', 'Food Preferences', )
 
     height = fields.Integer('Height', required=True, )
