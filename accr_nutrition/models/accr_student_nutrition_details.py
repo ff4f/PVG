b7dd5067839f55316f3a4667ca25500ad30e2766
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-15 21:01:09 +0400

    fix

--- accr_nutrition/models/accr_student_nutrition_details.py
+++ accr_nutrition/models/accr_student_nutrition_details.py
@@ -8,7 +8,7 @@ class accrStudentNutritionDetails(models.Model):
     name = fields.Char(related='student.display_name', string='Name', readonly=True, )
     age = fields.Integer(related='student.x_studio_age', string="Age", readonly=True, )
     diagnosis = fields.Text(related='student.x_studio_diagnosis', string='Diagnosis', readonly=True, )
-    residential_section = fields.Many2one(related='student.x_studio_residential_section', string='Residential Section', readonly=True, )
+    residential_section = fields.Many2one(related='student.x_studio_residential_sections', string='Residential Section', readonly=True, )
     medications = fields.One2many(related='student.x_studio_field_jm5yW', string='Medications', readonly=True, )
     food_preferences = fields.One2many('accr.food.preferences', 'student_nutrition_details', 'Food Preferences', )
 
