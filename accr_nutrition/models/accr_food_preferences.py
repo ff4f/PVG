73f3dbdf491560233a7e817b0e7e04070c828f82
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-16 11:58:53 +0400

    fix and update nutrition_details view

--- accr_nutrition/models/accr_food_preferences.py
+++ accr_nutrition/models/accr_food_preferences.py
@@ -5,10 +5,9 @@ class accrStudentFoodPreferences(models.Model):
     _name = "accr.food.preferences"
     _description = "Food prefernces menu for student"
 
-    student = fields.Many2one(
-        string=u'Student', comodel_name='model.x_student', ondelete='set restrict', required=True,)
-    student_nutrition_details = fields.Many2one('accr.student.nutrition.details', 'Nutrition Details',)
+    student = fields.Many2one('x_student', string=u'Student', required=True, ondelete='set restrict', )
+    student_nutrition_details = fields.Many2one('accr.student.nutrition.details', string=u'Nutrition Details',)
     name = fields.Char('Name', required=True,)
     preference = fields.Selection(
-        [('Like', 'Like'), ('Dislike', 'Dislike')], 'Preference', required=True,)
+        [('Like', 'Like'), ('Dislike', 'Dislike')], string=u'Preference', required=True,)
     intolerance = fields.Boolean(string=u'Intolerance',)
