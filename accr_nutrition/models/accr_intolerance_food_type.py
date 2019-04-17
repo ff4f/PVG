f1fa31d81452d40057fdae89acdb2599eb3dae73
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-17 16:21:46 +0400

    fix

--- accr_nutrition/models/accr_intolerance_food_type.py
+++ accr_nutrition/models/accr_intolerance_food_type.py
@@ -1,5 +1,6 @@
 from odoo import models, fields
 
-_name = "accr.intolerance.food.type"
+class accrIntoleranceFoodType(models.Model):
+    _name = "accr.intolerance.food.type"
 
-name = fields.Char(string="Food Type",)
+    name = fields.Char(string="Food Type",)
