df0515565b656fc5d8197fe81b3f9363246a6a97
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-18 15:57:22 +0400

    fix

--- accr_nutrition/models/accr_meal_timing.py
+++ accr_nutrition/models/accr_meal_timing.py
@@ -5,7 +5,7 @@ class accrMealTiming(models.Model):
     _name = "accr.meal.timing"
     _description = "Meal Timing"
 
-    name = fields.Char('Name',)
+    name = fields.Char(string=u"Meal Name")
     hour = fields.Selection(
         [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
          ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
@@ -19,9 +19,3 @@ class accrMealTiming(models.Model):
     
     meal_type = fields.Many2one('accr.meal.type', string=u"Meal Type", required=True, )
     food = fields.Many2many('accr.food','accr_food_meal_timing_rel', 'accr_meal_timing_id', 'accr_food_id', string="Food", required=True, )
-
-    @api.multi
-    @api.depends('meal_type', 'hour', 'minute')
-    def _compute_name(self):    
-        for record in self:
-            record.name = record.meal_type.display_name
