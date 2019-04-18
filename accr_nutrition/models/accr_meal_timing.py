95b1127a94620c9993cd2223c5fb7fbc5f489c89
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-18 14:38:16 +0400

    fix meal timing

--- accr_nutrition/models/accr_meal_timing.py
+++ accr_nutrition/models/accr_meal_timing.py
@@ -5,7 +5,7 @@ class accrMealTiming(models.Model):
     _name = "accr.meal.timing"
     _description = "Meal Timing"
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char('Name',)
     hour = fields.Selection(
         [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
          ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
