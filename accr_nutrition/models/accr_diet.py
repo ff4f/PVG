d02da3390c19c62bcb961c0c7ca149b644240a61
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-20 12:01:17 +0400

    diet view and menu

--- accr_nutrition/models/accr_diet.py
+++ accr_nutrition/models/accr_diet.py
@@ -4,3 +4,5 @@ class accrDiet(models.Model):
     _name = 'accr.diet'
 
     name = fields.Char(string=u'Diet')
+    students = fields.Many2many('x_student', 'x_student_diet_rel',
+                                'x_student_id', 'diet_id', string="Students", required=True, )
