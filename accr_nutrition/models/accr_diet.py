cda0822853514ea16b53c950743aeee5adf45a26
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-20 16:36:57 +0400

    fix

--- accr_nutrition/models/accr_diet.py
+++ accr_nutrition/models/accr_diet.py
@@ -3,6 +3,6 @@ from odoo import models, fields
 class accrDiet(models.Model):
     _name = 'accr.diet'
 
-    name = fields.Char(string=u'Diet')
+    name = fields.Char(string=u'Diet', required=True,)
     students = fields.Many2many('x_student', 'x_student_diet_rel',
                                 'diet_id', 'x_student_id', string="Students", required=True, )
