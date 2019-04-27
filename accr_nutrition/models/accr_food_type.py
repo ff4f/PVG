62993a40979380ecad60a7c6f383b3423f90a218
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-27 15:49:15 +0400

    update

--- accr_nutrition/models/accr_food_type.py
+++ accr_nutrition/models/accr_food_type.py
@@ -5,4 +5,3 @@ class accrFoodType(models.Model):
     _description = "ACCR Food Types"
 
     name = fields.Char(string="Food Type",)
-    # medical_contraindication = fields.Many2many('accr.food.type', 'accr_medical_contraindication_food_type_rel', 'food_type_id', 'medicine_id', string=u'Medicines')
