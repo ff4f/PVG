28553cd74495177d554b9e19e670f5838b10e9ac
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-29 17:48:33 +0400

    update

---
+++ accr_nutrition/models/x_student.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class x_student(models.Model):
+    
+    _inherit = ['x_student']
+
+    diet = students = fields.Many2many('x_student', 'x_student_diet_rel', 'x_student_id', 'diet_id', string="Diets", )
+    food_intolerance = fields.One2many('accr.student.food.intolerance', 'student', string=u'Food Intolerance', )          
+    food_preferences = fields.One2many('accr.student.food.preferences', 'student', string=u'Food Preferences', )                      
+    nutrition_details = fields.One2many('accr.student.nutrition.details', 'student', string=u'Nutrition Details', )
+    
