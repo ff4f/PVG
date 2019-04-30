84d2bda32389b06d9d148f39984f30f06cd37565
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-30 12:12:14 +0400

    rollback

--- accr_medical_unit/models/__init__.py
+++ accr_medical_unit/models/__init__.py
@@ -1,4 +1,4 @@
 from . import accr_medical_unit_medicine_side_effects
 from . import accr_medical_unit_medicine
 from . import accr_medical_unit_medication
-# from . import accr_student
+from . import accr_student
