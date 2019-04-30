d454ebbbc81cb239c87f3a584b246e4576eb9d51
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-30 14:17:17 +0400

    test fix

--- accr_medical_unit/models/__init__.py
+++ accr_medical_unit/models/__init__.py
@@ -1,5 +1,5 @@
 from . import accr_medical_unit_medicine_side_effects
 from . import accr_medical_unit_medicine
 from . import accr_medical_unit_medication
-# from . import accr_student
-# from . import accr_medical_medications
+from . import accr_student
+from . import accr_medical_medications
