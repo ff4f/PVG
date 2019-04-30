ea7aa222662fe3382577fc99f876f3906e3afe09
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-30 15:57:30 +0400

    full rollback

--- accr_medical_unit/__manifest__.py
+++ accr_medical_unit/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'ACCR Medical Unit',
-    'depends': ['base', 'mail', 'studio_customization'],
+    'depends': ['base', 'mail'],
     'application': True,
     'author': 'ACCR',
 
--- accr_medical_unit/models/accr_student.py
+++ accr_medical_unit/models/accr_student.py
@@ -1,6 +1,6 @@
 from odoo import models, fields
 
 class accrStudent(models.Model):
-    _inherit = ['x_student']
+    _inherit = ['184']
 
     student_notifications = fields.One2many('accr.medical.unit.medication', 'student', string=u'Medicines', )
