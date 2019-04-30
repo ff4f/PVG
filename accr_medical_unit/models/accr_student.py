a9a15e6cf4a6b1d5fcbf4af2e2a2ee99bdab00d1
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-04-30 15:42:34 +0400

    fix test

--- accr_medical_unit/models/accr_student.py
+++ accr_medical_unit/models/accr_student.py
@@ -2,6 +2,5 @@ from odoo import models, fields
 
 class accrStudent(models.Model):
     _inherit = ['x_student']
-    _name = 'accr.student'
 
     student_notifications = fields.One2many('accr.medical.unit.medication', 'student', string=u'Medicines', )
