193e637cf3ed8e611461f6fbca259041f1f70e7e
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-01 12:34:33 +0400

    fix

--- accr_nutrition/models/accr_student_nutrition_details.py
+++ accr_nutrition/models/accr_student_nutrition_details.py
@@ -34,9 +34,10 @@ class accrStudentNutritionDetails(models.Model):
     def _compute_medications_intolerance(self):
         for record in self:
             for medication in record.medications:
-                for medical_contraindication in medication.medicine.medical_contraindication:
-                    for food_type in medical_contraindication.food_types:
-                        record.food_intolerance.write(0, 0, {'student': record.student, 'nutrition_details': id, 'food_type': food_type})
+                for medicine in medication.medicine:
+                    for medical_contraindication in medicine.medical_contraindication:
+                        for food_type in medical_contraindication.food_types:
+                            record.food_intolerance.write(0, 0, {'student': record.student, 'nutrition_details': id, 'food_type': food_type})
 
 
                 
