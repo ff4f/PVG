ee399024036a22990d28d40dab505d208d597dd4
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-02 16:36:20 +0400

    test fix

--- accr_nutrition/models/accr_student_food_preferences.py
+++ accr_nutrition/models/accr_student_food_preferences.py
@@ -11,7 +11,7 @@ class accrStudentFoodPreferences(models.Model):
     student = fields.Many2one(related='nutrition_student.student', string=u'Student', required=True, )
     food = fields.Many2many('accr.food','accr_food_food_preferences_rel', 'food_preferences_id', 'accr_food_id', string="Food", required=True, )
     food_group = fields.Many2one('accr.food.group', string=u'Food Group', required=True, )
-    name = fields.Char('Name', compute='_compute_name', )
+    # name = fields.Char('Name', compute='_compute_name', )
     preference = fields.Selection(
         [('Like', 'Like'), ('Dislike', 'Dislike')], string=u'Preference', required=True,)
 
@@ -19,4 +19,4 @@ class accrStudentFoodPreferences(models.Model):
     @api.depends('student', 'food_group')
     def _compute_name(self):
         for record in self:
-            record.name = record.student + ' - ' + record.food_group.display_name + ' - ' + record.preference.Selection
+            record.name = record.student.display_name + ' - ' + record.food_group.display_name + ' - ' + record.preference.Selection
