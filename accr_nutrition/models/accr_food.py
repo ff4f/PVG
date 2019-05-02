8a96119c1eacf2ebe4cbaf6eb596348a0b08fe91
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-02 16:48:34 +0400

    add food type view relation, view and menu

--- accr_nutrition/models/accr_food.py
+++ accr_nutrition/models/accr_food.py
@@ -5,4 +5,5 @@ class accrFood(models.Model):
 
     name = fields.Char(string=u'Food', required=True, )
     food_group = fields.Many2one('accr.food.group', string=u'Food Group', required=True, )
+    food_type = fields.Many2one('accr.food.type', string=u'Food Type', required=True, )
     meal_type = fields.Many2one('accr.meal.type', string=u"Meal Type", required=True, )
