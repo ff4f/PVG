195ae88d9b7e1069b0db9c4e728077c9c2c3b89a
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-02 16:53:37 +0400

    fix

--- accr_nutrition/__manifest__.py
+++ accr_nutrition/__manifest__.py
@@ -12,6 +12,7 @@
         'security/students_nutrition_security.xml',
         'security/ir.model.access.csv',
         'views/accr_food_group.xml',
+        'views/accr_food_type.xml',
         'views/accr_student_nutrition_details.xml',
         'views/accr_meal_type.xml',        
         'views/accr_meal_timing.xml',
