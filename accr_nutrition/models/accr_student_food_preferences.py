782192ca510de51730f2b325f71190fa545654bc
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-02 16:26:37 +0400

    fix

--- accr_nutrition/models/accr_student_food_preferences.py
+++ accr_nutrition/models/accr_student_food_preferences.py
@@ -5,9 +5,10 @@ class accrStudentFoodPreferences(models.Model):
     _name = "accr.student.food.preferences"
     _description = "Food prefernces menu for student"
 
-    student = fields.Many2one('x_student', string=u'Student', required=True, )
+    # student = fields.Many2one('x_student', string=u'Student', required=True, )
     # nutrition_details = fields.Many2one('accr.student.nutrition.details', string=u'Nutrition Details',)
     nutrition_student = fields.Many2one('accr.nutrition.student', string=u'Nutrition Student',)
+    student = fields.Many2one(related='nutrition_student.student', string=u'Student', required=True, )
     food = fields.Many2many('accr.food','accr_food_food_preferences_rel', 'food_preferences_id', 'accr_food_id', string="Food", required=True, )
     food_group = fields.Many2one('accr.food.group', string=u'Food Group', required=True, )
     name = fields.Char('Name', compute='_compute_name', )
