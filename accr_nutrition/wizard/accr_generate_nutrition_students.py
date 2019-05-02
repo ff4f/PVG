adfcf504b2fa1d1205bf961a464f127fa122b987
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-03 00:04:06 +0400

    genrarot to add create nutrition students

---
+++ accr_nutrition/wizard/accr_generate_nutrition_students.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, api, _
+
+
+class accrGenerateNutritionStudents(models.TransientModel):
+    _name = "accr.generate.nutrition.students"
+    _description = "Generate Nutrition Students"
+
+
+    students = fields.Many2many('x_student', 'generate_get_students_rel', 'generate_id', 'student_id',  required=True, store=False )
+    nutrition_students = fields.Many2many('accr.nutrition.student', 'generate_set_nutrition_students_rel', 'generate_id', 'nutrition_student_id', store=False )
+
+    @api.multi
+    def act_gen_nutrition_students(self):
+        for record in self:
+            students = []
+            for student in record.students:
+                students.append({'student': student.id})
+            record.nutrition_students = self.env['accr.nutrition.student'].create(students)
+            return {'type': 'ir.actions.act_window_close'}
