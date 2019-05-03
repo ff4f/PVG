f1380cac3254ea273266d5e72f73998dfe6be19c
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-03 21:10:53 +0400

    .

--- accr_nutrition/models/accr_diet.py
+++ accr_nutrition/models/accr_diet.py
@@ -2,7 +2,6 @@ from odoo import models, fields
 
 class accrDiet(models.Model):
     _name = 'accr.diet'
-    _sql_constraints = [('student_unique', 'unique(students)', 'Can not be duplicate value for this field!')]
 
     name = fields.Char(string=u'Diet', required=True,)
     students = fields.Many2many('x_student', 'x_student_diet_rel',
