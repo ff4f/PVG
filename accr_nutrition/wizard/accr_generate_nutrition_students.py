cc04b0b0982d4cf31fbba9c644fb61e1ddbcf02e
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-03 13:49:52 +0400

    fix

--- accr_nutrition/wizard/accr_generate_nutrition_students.py
+++ accr_nutrition/wizard/accr_generate_nutrition_students.py
@@ -13,6 +13,6 @@ class accrGenerateNutritionStudents(models.TransientModel):
         for record in self:
             students = []
             for student in record.students:
-                students.append({'student': student.id})
-            self.env['accr.nutrition.student'].create(students)
+                # students.append({'student': student.id})
+                self.env['accr.nutrition.student'].create({'student': student.id})
             return {'type': 'ir.actions.act_window_close'}
