b5a3682ff42d4b4d2cbbd65f33f179cf9238125a
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-06 22:11:08 +0400

    ,

--- accr_nutrition/models/accr_nutrition_student.py
+++ accr_nutrition/models/accr_nutrition_student.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 class accrNutritionStudent(models.Model):
     _name = 'accr.nutrition.student'
     _description = 'Student Nutrition Details'
-    # _sql_constraints = [('student_unique', 'unique(student)', 'Can not be duplicate value for this field!')]
+    _sql_constraints = [('student_unique', 'unique(student)', 'Can not be duplicate value for this field!')]
 
     name = fields.Char(string=u'Name', compute='_compute_name', )
 
@@ -47,9 +47,9 @@ class accrNutritionStudent(models.Model):
     @api.multi
     @api.depends('student_birth_date')
     def _compute_age(self):
-        nowdate = datetime.datetime.today()
-        birthdate = self.student_birth_date
         for record in self:
+            nowdate = datetime.datetime.today()
+            birthdate = record.student_birth_date
             if record.student_birth_date:
                 record.student_age = str(nowdate.year - birthdate.year - ((nowdate.month, nowdate.day) < (birthdate.month, birthdate.day))) + ' years'
 
