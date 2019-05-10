63e2a2f9f744d7b4022bf90329fed3d3df2814f8
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-10 22:53:25 +0400

    .

--- accr_nutrition/models/accr_bca.py
+++ accr_nutrition/models/accr_bca.py
@@ -19,7 +19,7 @@ class accrBCA(models.Model):
     ecw = fields.Integer(string=u"ECW")
 
     nutrition_student = fields.Many2one('accr.nutrition.student', string=u"Student")
-    student = fields.Many2one(related='nutrition_student.student', string=u"Student")
+    student = fields.Many2one(related='nutrition_student.student', string=u"X Student")
 
     @api.multi
     @api.depends('student', 'create_date')
--- accr_nutrition/models/accr_nutrition_student.py
+++ accr_nutrition/models/accr_nutrition_student.py
@@ -10,6 +10,7 @@ class accrNutritionStudent(models.Model):
                          'Can not be duplicate value for this field!')]
 
     name = fields.Char(string=u'Name', compute='_compute_name', readonly=True )
+    id = fields.Integer(string=u'ID', compute='_compute_id', readonly=True, )
 
     student = fields.Many2one('x_student', string=u'Student', required=True,
                               index=True, store=True, track_visibility='onchange', )
@@ -77,6 +78,12 @@ class accrNutritionStudent(models.Model):
     def _compute_name(self):
         for record in self:
             record.name = record.student.display_name
+    
+    @api.multi
+    @api.depends('student')
+    def _compute_id(self):
+        for record in self:
+            record.id = record.student.id
 
     @api.multi
     @api.depends('student_birth_date')
