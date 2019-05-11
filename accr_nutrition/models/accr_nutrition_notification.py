ae4962dca3f7472dcc008456b3c0c4b3544fd7ce
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-11 22:12:49 +0400

    .

--- accr_nutrition/models/accr_nutrition_notification.py
+++ accr_nutrition/models/accr_nutrition_notification.py
@@ -5,7 +5,7 @@ class accrNutritiontNotification(models.Model):
     _description = 'Nutrition Notifications'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(sting=u'Name', )
+    name = fields.Char(sting=u'Name', compute='_compute_name', )
     student = fields.Many2one('accr.nutrition.student', string=u'student', required=True, readonly=True, )
     color = fields.Integer(string=u'Color')
     message = fields.Text(string=u'Message', readonly=True, )
@@ -17,4 +17,4 @@ class accrNutritiontNotification(models.Model):
     def _compute_name(self):
         for record in self:
             if record.student:
-                record.name = record.student.display_name + ' - ' + record.create_date.strftime("%Y-%m-%d")
+                record.name = record.student.display_name + ' - ' + record.notification_domain + ' - '+ record.create_date.strftime("%Y-%m-%d")
