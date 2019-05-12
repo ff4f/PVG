48bf517eece4eb30ce909b5722f5e7baab9d2a97
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-13 03:22:03 +0400

    !

--- accr_nutrition/models/accr_food_group.py
+++ accr_nutrition/models/accr_food_group.py
@@ -4,4 +4,4 @@ class accrFoodGroup(models.Model):
     _name = 'accr.food.group'
 
     name = fields.Char(string=u'Food Group', required=True, )
-    food = fields.Many2many('accr.food', 'food_group', 'accr_food_group_food_rel', 'food_id', 'food_group_id', string=u'foods', required=True, )
+    food = fields.Many2many('accr.food', 'accr_food_group_food_rel', 'food_id', 'food_group_id', string=u'foods', required=True, )
