94a0a8400a7610c7b4cefb23b5348880c9048916
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-05-13 03:20:21 +0400

    .

--- accr_nutrition/models/accr_food_ingredients.py
+++ accr_nutrition/models/accr_food_ingredients.py
@@ -5,5 +5,5 @@ class accrFoodType(models.Model):
     _description = "ACCR Food Ingredientss"
 
     name = fields.Char(string="Food Ingredients", required=True, )
-    food = fields.One2many('accr.food', 'food_ingredients', string=u'Food', )
+    food = fields.Many2many('accr.food', 'accr_food_ingredients_food_rel', 'food_id', 'food_ingredients_id', string=u'Food', required=True, )
     medical_contraindication = fields.Many2many('accr.medical.contraindication', 'accr_medical_contraindication_food_ingredients_rel', 'food_ingredients_id', 'contraindication_id', string=u'Food Ingredientss', required=True)
