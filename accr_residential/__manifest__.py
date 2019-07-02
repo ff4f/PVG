1bc1c88665ab2a5877d6aeeb187a29c68a38c829
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-07-02 17:08:02 +0400

    Update __manifest__.py

--- accr_residential/__manifest__.py
+++ accr_residential/__manifest__.py
@@ -12,6 +12,6 @@
         'security/accr_residential_security.xml',
         'security/ir.model.access.csv',
         'views/residential_nm_notification_view.xml',
-        'menus/accr_residential_security.xml',
+        'menus/accr_residential_menu.xml',
     ],
 }
