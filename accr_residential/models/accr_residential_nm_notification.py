872a12cbaecf8ff8c0777f75b7839ec3c5a435d6
husseinAliPVG <43534139+husseinAliPVG@users.noreply.github.com>
2019-07-02 17:15:38 +0400

    Update accr_residential_nm_notification.py

--- accr_residential/models/accr_residential_nm_notification.py
+++ accr_residential/models/accr_residential_nm_notification.py
@@ -5,11 +5,11 @@ class accrResidentialNMNotification(models.Model):
     _description = "Resdintial Notifications for Medical and Nutrition"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string=u'Subject', )
-    date_time = fields.Datetime(string=u'Date & Time', default=fields.Datetime.now,)
-    notes = fields.Text(string=u'Notes',)
+    name = fields.Char(string=u'Subject', required=True, )
+    date_time = fields.Datetime(string=u'Date & Time', default=fields.Datetime.now, required=True, )
+    notes = fields.Text(string=u'Notes', required=True, ) 
     
-    section = fields.Many2one(string=u'Section', comodel_name='x_student_residential_sections', ondelete='set null',)
+    section = fields.Many2one(string=u'Section', comodel_name='x_student_residential_sections', ondelete='set null', required=True, )
     students = fields.Many2many(string=u'Students', comodel_name='x_student', relation='student_residential_nm_notification_rel', column1='student_id', column2='residential_nm_notification_id',)
     
     
