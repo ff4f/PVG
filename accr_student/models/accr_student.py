a5f68669930c4f4176b42b99290115dc200a65a6
Mohamed Saber <mohamedabosaber94@gmail.com>
2020-10-28 01:18:18 +0200

    [Fix]Plan

--- accr_student/models/accr_student.py
+++ accr_student/models/accr_student.py
@@ -4,3 +4,9 @@ class accrStudent(models.Model):
     _inherit = ['x_student']
 
     student_notifications = fields.One2many('accr.student.notification', 'student', string=u'Notifications', )
+
+
+class LongPlan(models.Model):
+    _inherit = ['x_se_long_term_plan']
+
+    write_date = fields.Datetime("Last Updated On", readonly=False, index=True)
