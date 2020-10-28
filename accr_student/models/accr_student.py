ff12618afb679b30db83c2660c6514174ffc7303
Mohamed Saber <mohamedabosaber94@gmail.com>
2020-10-28 16:29:06 +0200

    [Fix]Plan

--- accr_student/models/accr_student.py
+++ accr_student/models/accr_student.py
@@ -1,9 +1,9 @@
 from odoo import models, fields, _
 
-class accrStudent(models.Model):
-    _inherit = ['x_student']
-
-    student_notifications = fields.One2many('accr.student.notification', 'student', string=u'Notifications', )
+# class accrStudent(models.Model):
+#     _inherit = ['x_student']
+#
+#     student_notifications = fields.One2many('accr.student.notification', 'student', string=u'Notifications', )
 
 
 class LongPlan(models.Model):
